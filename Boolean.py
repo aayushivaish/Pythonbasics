9d52da4d5d025a93f1767560dc787b1513a5356a
aayushivaish <aayushi.prakhar@gmail.com>
2020-11-27 14:23:03 +0530

    Boolean

---
+++ Boolean.py
@@ -0,0 +1,40 @@
+a = 9
+b = 11
+c = 0
+ 
+if ( a == b ):
+   print ("a is equal to b")
+else:
+   print ("a is not equal to b")
+ 
+if ( a != b ):
+   print ("a is not equal to b")
+else:
+   print ("a is equal to b")
+ 
+if ( a != b ):
+   print ("a is not equal to b")
+else:
+   print ("a is equal to b")
+ 
+if ( a < b ):
+   print ("a is less than b")
+else:
+   print ("a is not less than b")
+ 
+if ( a > b ):
+   print ("a is greater than b")
+else:
+   print ("a is not greater than b")
+ 
+a = 6
+b = 18
+if ( a <= b ):
+   print ("a is either less than or equal to  b")
+else:
+   print ("a is neither less than nor equal to  b")
+ 
+if ( b >= a ):
+   print ("b is either greater than  or equal to b")
+else:
+   print ("b is neither greater than  nor equal to b")
