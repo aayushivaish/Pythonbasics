d660c0dfc696b6aeb9b293def0e9543a697a8abd
Prakhar Varshney <prakharnegative@gmail.com>
2021-02-04 23:15:37 +0530

    Create primepartition.py

---
+++ nptl/primepartition.py
@@ -0,0 +1,28 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+def primepartition(m):
+    prime_numbers = list_prime(m)
+    for i in range()
+    
+    
+    
+    
+
+def list_prime(m):
+    prime_list = []
+    for i in range(2,m):
+        is_prime = True
+        for j in range(2,i):
+            if i % j == 0:
+                is_prime = False
+                break
+        if is_prime == True:
+            prime_list.append(i)
+    return prime_list
+
+
+
+
+
+                
+                
